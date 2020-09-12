a0c9678fb7eddcb21a954209bf6c83e216b01e89
Andrii Hromov <andrii.hromov@gmail.com>
2020-09-12 20:37:53 +0300

    A small code cleanup

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,6 +11,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
   hero: Hero;
 
   constructor(
