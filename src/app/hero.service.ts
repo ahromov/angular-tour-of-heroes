147787a6fee457ed8432dcf1de068a1c4e138179
Andrii Hromov <andrii.hromov@gmail.com>
2020-09-12 14:45:12 +0300

    Refactor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,11 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 }
